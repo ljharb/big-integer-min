78841273b0757f3a8e647837b54447a788c0f6c4
Jordan Harband <ljharb@gmail.com>
2013-11-01 16:13:08 -0700

    Adding another test

--- test.js
+++ test.js
@@ -30,6 +30,7 @@ test('works', function (t) {
 	t.equal(bigIntMin('10', '100'), '10', 'works with numbers of different lengths');
 	t.equal(bigIntMin('0', '3'), '0', 'works with zero');
 	t.equal(bigIntMin('123', '123'), '123', 'works with the same value');
+	t.equal(bigIntMin('-123', '-123'), '-123', 'works with the same negative value');
 
 	var reallyBigInteger = '179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368';
 	t.equal(bigIntMin(reallyBigInteger, '0'), '0', 'works with a really big integer');
